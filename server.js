844c028c58c7f24d44438cff0dab8c63df24f872
putty10 <putneyryan@gmail.com>
2023-01-30 19:56:39 -0500

    handlebars and routes

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controller');
 // import sequelize connection
 const sequelize = require('./config/connection');
+// import express-session
+const session = require('express-session');
+// import express-handlebars
+const exphbs = require('express-handlebars');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// set up session
+const sess = {
+  secret: 'What it Is?',
+  cookies: {
+    httpOnly: true,
+    secure: false,
+    samesite: 'strict',
+    maxage: null,
+  }
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
